57352114bb30911b611c031affcdaad20da7c7a4
disaacjm <disaacjm@gmail.com>
2023-07-14 00:36:43 +0100

    all done but miss readme

---
+++ client/src/components/PhoneCardComponent.js
@@ -0,0 +1,25 @@
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+
+function PhoneCardComponent({
+  id,
+  name,
+  manufacturer,
+  description,
+  imageFileName,
+}) {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={`/images/${imageFileName}`} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>{manufacturer}</Card.Text>
+        <Card.Text>{description}</Card.Text>
+        <Link to={`/phones/${id}`} className="btn btn-secondary">view details</Link>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default PhoneCardComponent;
--- server/app.js
+++ server/app.js
@@ -16,7 +16,10 @@ require("./config")(app);
 
 // 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
-app.use("/api", indexRoutes);
+app.use("/", indexRoutes);
+
+app.use("/api", require("./routes/phone.routes"));
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
