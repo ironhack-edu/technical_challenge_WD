da0bf68784101154c10f878d5b682ab75726b9c9
Filipe Ferreira <filipeferreira94@gmail.com>
2023-07-13 10:44:59 +0100

    fix(app): removed unneded imports

--- phonecave-server/app.js
+++ phonecave-server/app.js
@@ -3,7 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var phonesRouter = require('./routes/phones');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -15,7 +14,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/phones', phonesRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
