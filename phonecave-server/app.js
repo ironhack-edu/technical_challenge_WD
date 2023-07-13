5574058a7d0e7153ebf70a3bd8a65882381b4c6b
Filipe Ferreira <filipeferreira94@gmail.com>
2023-07-13 11:04:51 +0100

    feat(app): Allow request from all origins with CORS

--- phonecave-server/app.js
+++ phonecave-server/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +14,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Allow request from any origin
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
