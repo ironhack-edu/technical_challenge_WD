cfdec76fd4be5a1db20f992e217846a6b7dca2b4
Filipe Ferreira <filipeferreira94@gmail.com>
2023-07-13 11:05:28 +0100

    feat(API): set up api URL

--- phonecave-client/src/App.jsx
+++ phonecave-client/src/App.jsx
@@ -1,18 +1,20 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import API_URL from './apiConfig';
 
 function App() {
-	const [phones, setPhones] = useState([]);
+	const [phones, setPhones] = useState(null);
 	const [loading, setLoading] = useState(true);
 
 	const getPhones = async () => {
 		try {
-			const response = await axios.get('/phones');
+			const response = await axios.get(`${API_URL}/phones`);
 			console.log('response.data: ', response.data);
 			setPhones(response.data);
 			setLoading(false);
 		} catch (error) {
-			console.error('Error gettng list of phones:', error);
+			console.error('Error getting list of phones:', error);
 		}
 	};
 
@@ -20,7 +22,22 @@ function App() {
 		getPhones();
 	}, []);
 
-	return <>{loading && <div>Loading...</div>}</>;
+	/* getPhones(); */
+
+	console.log('phones: ', phones);
+
+	return (
+		<>
+			{loading && <div>Loading...</div>}
+			{phones && (
+				<section>
+					{phones.map((phone) => {
+						<div key={phone.id}>Phone: {phone.name}</div>;
+					})}
+				</section>
+			)}
+		</>
+	);
 }
 
 export default App;
