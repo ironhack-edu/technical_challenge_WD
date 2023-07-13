6a4c3b329531fa6031a7983efb7d759123e733f3
Filipe Ferreira <filipeferreira94@gmail.com>
2023-07-13 11:44:08 +0100

    all iterations finished

--- phonecave-client/src/App.jsx
+++ phonecave-client/src/App.jsx
@@ -2,41 +2,75 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 import API_URL from './apiConfig';
+import { Button, Container, Flex, Loader, Popover, Text } from '@mantine/core';
 
 function App() {
 	const [phones, setPhones] = useState(null);
 	const [loading, setLoading] = useState(true);
 
-	const getPhones = async () => {
-		try {
-			const response = await axios.get(`${API_URL}/phones`);
-			console.log('response.data: ', response.data);
-			setPhones(response.data);
-			setLoading(false);
-		} catch (error) {
-			console.error('Error getting list of phones:', error);
-		}
-	};
-
 	useEffect(() => {
+		const getPhones = async () => {
+			try {
+				setLoading(true);
+				console.log(loading);
+				const response = await axios.get(`${API_URL}/phones`);
+				setPhones(response.data);
+				setLoading(false);
+			} catch (error) {
+				console.error('Error getting list of phones:', error);
+			} /* finally {
+				setLoading(false);
+				console.log(loading);
+			} */
+		};
+
 		getPhones();
 	}, []);
-
-	/* getPhones(); */
 
 	console.log('phones: ', phones);
 
 	return (
-		<>
-			{loading && <div>Loading...</div>}
+		<Container>
+			{loading === true && <Loader />}
 			{phones && (
-				<section>
-					{phones.map((phone) => {
-						<div key={phone.id}>Phone: {phone.name}</div>;
-					})}
-				</section>
+				<Flex
+					mih={50}
+					gap="md"
+					justify="center"
+					/* align="center" */
+					direction="row"
+					wrap="wrap"
+				>
+					<section>
+						{phones.map((phone) => (
+							<Popover
+								m={'sm'}
+								key={phone.id}
+								width={200}
+								position="bottom"
+								withArrow
+								shadow="md"
+							>
+								<Popover.Target>
+									<Button>{phone.name}</Button>
+								</Popover.Target>
+								<Popover.Dropdown>
+									<Text size="sm">
+										Name: {phone.name}
+										Manufacturer: {phone.manufacturer}
+										{phone.description}
+										Color: {phone.color}
+										Price: {phone.price}€ Screen: {phone.screen}
+										Processor: {phone.processor}
+										Ram: {phone.ram}
+									</Text>
+								</Popover.Dropdown>
+							</Popover>
+						))}
+					</section>
+				</Flex>
 			)}
-		</>
+		</Container>
 	);
 }
 
--- phonecave-client/src/main.jsx
+++ phonecave-client/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+import { MantineProvider } from '@mantine/core';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<MantineProvider withGlobalStyles withNormalizeCSS>
+			<App />
+		</MantineProvider>
+	</React.StrictMode>
+);
