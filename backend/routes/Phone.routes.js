d4153a73ba9d24c0c0049ab859501054bde24d03
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2023-08-10 11:45:25 +0200

    backend git ignore issues solved new backend

---
+++ backend/routes/Phone.routes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const phonesData = require("../data/phones.json");
+
+router.get("/", async (req,res) => {
+    try {
+        res.json(phonesData);
+        
+    } catch (error) {
+        console.log("getting phones failed", error)
+        
+    }
+});
+
+router.get("/:id", (req, res) => {
+    const phoneId = parseInt(req.params.id);
+    const phone = phonesData.find(phone => phone.id === phoneId);
+
+    if (phone) {
+        res.json(phone);
+    } else {
+        res.status(404).json({ message: "Phone not found" });
+    }
+});
+
+module.exports = router;
