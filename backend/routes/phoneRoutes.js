607cffe694563404a6e2a47a39e0e247d9f8c244
Biljana Kotevska <biljanakotev@gmail.com>
2023-11-08 12:08:45 +0100

    Remove node_modules from version control

--- backend/routes/phoneRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const phonesData = require('../data/phones.json');
-
-router.get('/', (req, res) => {
-  res.json(phonesData);
-});
-
-module.exports = router;
