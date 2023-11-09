13415f1dbdba719971311ddcad38357114d01038
thomas demoncy <demoncy.thomas@gmail.com>
2023-11-09 19:07:43 +0100

    changed assets directory + api call to get all phones.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const phones = require("../data/phones.json");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+app.use(cors({origin:
+    ["http://localhost:5173/",
+    "http://localhost:5173"]
+  }))
 
 app.get('/phones', (req, res) => {
     res.status(201).json(phones)
