2cf7b9c20d51529f83ee813bc77e3653eb28391d
thomas demoncy <demoncy.thomas@gmail.com>
2023-11-09 17:53:20 +0100

    /phones/:id route done

--- server/app.js
+++ server/app.js
@@ -7,8 +7,15 @@ app.use(express.json());
 app.get('/phones', (req, res) => {
     res.status(201).json(phones)
 })
+
+app.get('/phones/:id', (req, res) => {
+    const { id } = req.params;
+    const detailPhone = phones.find((phone) => phone.id === parseInt(id));
+    res.status(201).json(detailPhone)
+})
+
 const server = app.listen(4000, () => {
     console.log(`Server listening on port 4000`);
-  });
-  
-  module.exports = server
+});  
+
+module.exports = server
