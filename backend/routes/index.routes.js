85e2e8cae97abe7fc5f15c38a2fd06902e92a713
JanJaap <j-jschenk@hotmail.com>
2023-11-10 12:52:46 +0100

    first model and route up and running

--- backend/routes/index.routes.js
+++ backend/routes/index.routes.js
@@ -1,11 +1,23 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const Phone = require("../models/Phone.model");
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
 // /phones get show all phones
-
+router.get("/phones", async(req, res, next) => {
+ try{
+  const phones = await Phone.find()
+  res.status(200).json(phones)
+} catch (error) {
+  res
+    .status(500)
+    .json({ error: "Status code: 500 (Internal Server Error)" });
+}
+})
 // /phones/:id get 1 phone   
 
 module.exports = router;
