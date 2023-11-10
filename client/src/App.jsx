7bdee911111754e1230bbdf9ad5edbb138a0eee9
thomas demoncy <demoncy.thomas@gmail.com>
2023-11-10 09:38:07 +0100

    added mantine to project + spinner when /phones call

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from 'react'
+import {Loader} from "@mantine/core"
 import './App.css'
 import axios from "axios";
 
 
 function App() {
   const [phones, setPhones] = useState();
+  const [isLoading, setIsLoading] = useState(true);
   
   const getAllPhones = () => {
     axios.get(`${import.meta.env.VITE_API_URL}/phones`)
     .then((response) => {
       setPhones(response)
+      setIsLoading(false)
     })
     .catch((error) => {
       console.log(error)
@@ -20,7 +23,11 @@ function App() {
     getAllPhones()
   }, [])
 
-  return (
+  return  isLoading ? (
+    <>
+      <Loader color="#288BE2" />
+    </>
+  ) : (
     <>
       <h1>The best phones:</h1>
     </>
