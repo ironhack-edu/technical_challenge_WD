ff780e507404939d9803cd86d38af8b74049b6e2
JanJaap <j-jschenk@hotmail.com>
2023-11-10 13:52:07 +0100

    first page, some quick styling

--- backend/routes/index.routes.js
+++ backend/routes/index.routes.js
@@ -7,7 +7,7 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
-// /phones get show all phones
+
 router.get("/phones", async(req, res, next) => {
  try{
   const phones = await Phone.find()
@@ -18,6 +18,26 @@ router.get("/phones", async(req, res, next) => {
     .json({ error: "Status code: 500 (Internal Server Error)" });
 }
 })
-// /phones/:id get 1 phone   
+   
+router.get("/phones/:id", async (request, response) => {
+  const { id } = request.params;
+  if (mongoose.isValidObjectId(id)) {
+    try {
+      const currentPhone = await Phone.findById(id);
+      if (currentPhone) {
+        response.json(currentPhone);
+      } else {
+        response
+          .status(404)
+          .json({ message: "Phone not found" });
+      }
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error });
+    }
+  } else {
+    response.status(400).json({ message: "The id seems wrong" });
+  }
+});
 
 module.exports = router;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,53 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [phones, setPhones] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const API_URL = import.meta.env.VITE_API_URL;
+
+  const fetchPhones = () => {
+    fetch(`${API_URL}/api/phones`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setPhones(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchPhones();
+  }, []);
 
   return (
     <>
       <div>
-        <img src="/Galaxy_S7.png" />
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>List of available phones</h1>
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <div className="phoneContainer">
+            {phones.map((phone) => (
+              <div key={phone.id}>
+                <img
+                  src={`/${phone.imageFileName}`}
+                  className="phone-image"
+                  alt={`Image of ${phone.name}`}
+                />
+                <p>{`${phone.name}`}</p>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
