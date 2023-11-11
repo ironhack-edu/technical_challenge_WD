5b91f6cb032ae685163fbb558d80a50fbadbc7ec
franklinosei <franklinosei15@gmail.com>
2023-11-11 15:41:54 +0000

    work done

--- client/src/pages/PhoneDetailsPage/PhoneDetailsPage.jsx
+++ client/src/pages/PhoneDetailsPage/PhoneDetailsPage.jsx
@@ -12,7 +12,8 @@ function PhoneDetailsPage() {
     try {
       setIsLoading(true);
       const response = await apiConnect.getPhoneById(id);
-      setPhoneDetails(response.data);
+      console.log(response.data);
+      setPhoneDetails(response?.data[0]);
       setIsLoading(false);
     } catch (err) {
       const { response } = err;
@@ -27,8 +28,46 @@ function PhoneDetailsPage() {
   }, [id]);
 
   return (
-    <div>
-      <h1>Phone details</h1>
+    <div className="container mx-auto mt-8">
+      {isLoading ? (
+        ""
+      ) : (
+        <div className="max-w-2xl mx-auto bg-white p-8 shadow-lg">
+          <div className="flex justify-between items-center mb-4">
+            <h1 className="text-3xl font-bold">{phoneDetails?.name}</h1>
+            <span className="text-2xl font-bold text-blue-500">
+              ${phoneDetails?.price}
+            </span>
+          </div>
+          <div className="flex">
+            <img
+              className="w-1/2 rounded-lg mr-4"
+              src={phoneDetails?.imageFileName && require(`../../assets/images/${phoneDetails?.imageFileName}`)}
+              alt={`${phoneDetails?.name} - ${phoneDetails?.color}`}
+            />
+            <div className="w-1/2">
+              <p className="text-gray-700 mb-4">{phoneDetails?.description}</p>
+              <ul className="list-disc list-inside">
+                <li>
+                  <strong>Manufacturer:</strong> {phoneDetails?.manufacturer}
+                </li>
+                <li>
+                  <strong>Color:</strong> {phoneDetails?.color}
+                </li>
+                <li>
+                  <strong>Screen:</strong> {phoneDetails?.screen}
+                </li>
+                <li>
+                  <strong>Processor:</strong> {phoneDetails?.processor}
+                </li>
+                <li>
+                  <strong>RAM:</strong> {phoneDetails?.ram}GB
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
