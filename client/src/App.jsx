ac6ec15f1e90264054cd5b825303f1294e490685
Daniel Rodrigues <danielbrodrigues25@gmail.com>
2023-12-18 19:19:27 +0000

    Done

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PhoneList from './Components/PhoneList';
+
+const App = () => {
+    return (
+        <div>
+            <PhoneList />
+        </div>
+    );
+};
+
+export default App;
