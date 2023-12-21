ba574e70bbeba0efc15e3967f297417f0911b7c0
Fran Martinez <pmg.webdev@gmail.com>
2023-12-21 20:39:06 +0100

    creates get request for a phone id

--- technical_challenge_WD/server/routes/phones.routes.js
+++ technical_challenge_WD/server/routes/phones.routes.js
@@ -7,4 +7,16 @@ router.get("/", (req, res) => {
   res.json(phonesJson);
 });
 
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+
+    const phone = phonesJson.find(phone => phone.id === parseInt(id, 10))
+
+    if(!phone) {
+        return res.status(404).json({message: "Phone not found"})
+    }
+
+    res.json(phone)
+})
+
 module.exports = router;
