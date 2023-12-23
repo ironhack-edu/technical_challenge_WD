28b1f70d0f20fd0c46a00f378f4b9566a08f3f62
brrrun <brunomsantosmendes@gmail.com>
2023-12-23 22:32:05 +0000

    completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,11 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Homepage from './Components/Homepage'
-import AllPhones from './Components/AllPhones'
+import PhoneList from './Components/PhoneList'
+
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Homepage/>}/>
-        <Route path="/phones" element={<AllPhones/>}/>
-      </Routes>
+      <PhoneList/>
     </div>
   )
 }
